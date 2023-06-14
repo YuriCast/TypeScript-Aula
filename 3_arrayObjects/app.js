2aa89cc81be3f88fe0f0bebcd3e8b0cc4c078068
YURI CASTILHO <yuri_castilho1@estudante.sesisenai.org.br>
2023-06-14 09:18:20 -0300

    arrayObjects

---
+++ 3_arrayObjects/app.js
@@ -0,0 +1,5 @@
+var fruits = ['Banana', 'Apple', 'Orange'];
+fruits.push('Banana');
+var numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+console.log(fruits);
---
+++ 3_arrayObjects/app.ts
@@ -0,0 +1,7 @@
+let fruits: string[] = ['Banana', 'Apple', 'Orange']
+fruits.push('Banana')
+
+let numbers: array<number> [] = [1, 2, 3, 4, 5]
+
+console.log(numbers)
+console.log(fruits)
