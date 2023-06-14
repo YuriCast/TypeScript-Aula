bc6bc7c554e53e4c8a878f4df00dd67b74230af6
YURI CASTILHO <yuri_castilho1@estudante.sesisenai.org.br>
2023-06-14 08:57:40 -0300

    HelloWorld

---
+++ 1_helloworld/app.js
@@ -0,0 +1,5 @@
+function helloWorld(name) {
+    return 'Hello World ' + name;
+}
+var a = 'Yasmin Castilho';
+console.log(helloWorld(a));
---
+++ 1_helloworld/app.ts
@@ -0,0 +1,6 @@
+function helloWorld(name: string) {
+    return 'Hello World ' + name 
+}
+
+let a = 'Yasmin Castilho'
+console.log(helloWorld(a))
