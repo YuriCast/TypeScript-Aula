2f5588a359195524abefcb520da60fff5ae2645d
YURI CASTILHO <yuri_castilho1@estudante.sesisenai.org.br>
2023-06-14 09:37:30 -0300

    any

--- 5_enum/app.js
+++
@@ -1,3 +0,0 @@
-var plane;
-plane = ['Beeing 737', true, 5, '2 turbines'];
-console.log(plane);
---
+++ 6_any/app.ts
@@ -0,0 +1,8 @@
+let euNaosei : any
+euNaosei = 'Test'
+euNaosei = 3
+euNaosei = true
+console.log(euNaosei)
+
+let undefinedList: any[] = ['Test', 3, true]
+console.log(undefinedList)
